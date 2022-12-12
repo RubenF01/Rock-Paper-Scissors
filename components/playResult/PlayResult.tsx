0acbf86b6f8f09257076257df7b0034faf03789c
RubenF01 <rubenfrias10@gmail.com>
2022-12-12 17:20:41 -0400

    modified text sizing and spacing in play result

--- components/playResult/PlayResult.tsx
+++ components/playResult/PlayResult.tsx
@@ -35,18 +35,20 @@ const PlayResult = ({
   };
 
   return (
-    <div className="flex items-center mt-24">
+    <div className="flex items-center mt-24 space-x-16">
       <div className="flex flex-col items-center">
-        <h1 className="text-white uppercase">you picked</h1>
+        <h1 className="pb-16 text-2xl font-bold text-white uppercase">
+          you picked
+        </h1>
         <Shape shapeInfo={playerShape as ShapeInfo} hasPlayed={hasPlayed} />
       </div>
 
-      <div className="flex flex-col items-center">
-        <h1 className="pb-4 text-4xl font-bold text-white uppercase">
+      <div className="flex flex-col items-center mt-10">
+        <h1 className="pb-4 text-5xl font-bold text-white uppercase">
           {formatResult(result)}
         </h1>
         <button
-          className="uppercase bg-white hover:text-[#9f1535] px-10 py-2 rounded-lg font-bold"
+          className="uppercase bg-white hover:text-[#9f1535] px-14 py-3 rounded-lg font-bold"
           onClick={() => resetGame()}
         >
           play again
@@ -54,7 +56,9 @@ const PlayResult = ({
       </div>
 
       <div className="flex flex-col items-center">
-        <h1 className="text-white uppercase">the house picked</h1>
+        <h1 className="pb-16 text-2xl font-bold text-white uppercase">
+          the house picked
+        </h1>
         <Shape shapeInfo={computerShape as ShapeInfo} hasPlayed={hasPlayed} />
       </div>
     </div>
