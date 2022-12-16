249b8c600938a1c0bc55697a2cf81209b3cc1cf5
RubenF01 <rubenfrias10@gmail.com>
2022-12-15 20:03:59 -0400

    fixed score box sizing for mobile

--- components/score/Score.tsx
+++ components/score/Score.tsx
@@ -1,18 +1,23 @@
 import Logo from "../../public/icons/logo.svg";
+import { useMedia } from "react-use";
 
 type Props = {
   score: number;
 };
 
 const Score = ({ score }: Props) => {
+  const isWide = useMedia("(min-width: 1280px)", false);
+
   return (
-    <div className="flex justify-between items-center border-2 w-80 xl:w-[700px] border-white/50 rounded-2xl pr-5 pl-7 cursor-default">
-      <div className="py-5">
-        <Logo />
+    <div className="flex justify-between items-center border-2 w-80 xl:w-[700px] border-white/50 rounded-lg xl:rounded-2xl pr-3 xl:pr-5 xl:pl-7 cursor-default">
+      <div className="xl:py-5">
+        <Logo className={`${isWide || "scale-50 -ml-5"}`} />
       </div>
 
-      <div className="bg-white h-[75%] w-36 flex items-center flex-col justify-center font-bold rounded-lg">
-        <p className="uppercase text-bellFlower">score</p>
+      <div className="bg-white h-[75%] w-20 xl:w-36 flex items-center flex-col justify-center font-bold rounded-lg">
+        <p className="text-[10px] uppercase text-bellFlower xl:text-base">
+          score
+        </p>
         <h1 className="text-5xl text-freefall">{score}</h1>
       </div>
     </div>
